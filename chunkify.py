2968d22ea2c6a871066cedc3042402e162fec914
Vigneshwar Hariharan <88077703+vigonometry@users.noreply.github.com>
2021-12-08 21:38:09 +0800

    Add files via upload

--- chunkify.py
+++ chunkify.py
@@ -1,4 +1,6 @@
 import constants as const
+with open("names.txt") as file:
+    lines = [line.strip('\n') for line in file]
 
 def split_chunks(message):
     chunks = []
@@ -13,23 +15,20 @@ def split_chunks(message):
             curr_line.append(line)
         else:
             chunks.append(curr_line)
-            if '\n' in line:
-                split_str = line.split('\n')
-                chunks.append([split_str[0]])
-                curr_line = [split_str[1:]]
+            loc = line.rfind(' ')
 
-                for section in split_str[1:]:
-                    num_chars += len(section)
+            if 0 <= loc < const.MAX_CHUNK_SIZE:
+                chunks.append([line[:loc]])
+                curr_line = [line[loc:]]
+                split_chunks(curr_line)
             else:
-                loc = line.rfind(' ')
-                if 0 <= loc < const.MAX_CHUNK_SIZE:
-                    chunks.append([line[:loc]])
-                    curr_line = [line[loc:]]
-                    split_chunks(curr_line)
-                else:
-                    chunks.append(line[:const.MAX_CHUNK_SIZE])
-                    curr_line = [line[const.MAX_CHUNK_SIZE:]]
-                    split_chunks(curr_line)
+                chunks.append(line[:const.MAX_CHUNK_SIZE])
+                curr_line = [line[const.MAX_CHUNK_SIZE:]]
+                split_chunks(curr_line)
     chunks.append(curr_line)
 
     return chunks
+
+chunkz = split_chunks(lines)
+for chunk in chunkz:
+    print(chunk)

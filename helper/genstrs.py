8f37b577c71c800313510aab64669a1c51c0088c
Vigneshwar Hariharan <88077703+vigonometry@users.noreply.github.com>
2021-12-09 18:56:28 +0800

    Update genstrs.py

--- helper/genstrs.py
+++ helper/genstrs.py
@@ -1,10 +1,31 @@
+from contextlib import redirect_stdout
 import random
 import string
-
-def generate_text():
+"""
+This function generates random text using letters and digits from the ascii libraries and adds whitespaces and newline characters at random intervals
+and exports the print output as a .txt file
+"""
+def generate_text_newline():
     numChars = int(input("Input desired length of string: " ))
     ref = string.ascii_letters + string.digits + '\n' + ' '
-    print(ref)
     newstr = random.choices(ref, k = numChars)
+    res = ''.join(newstr)
 
-    return ''.join(newstr)
+    with open('out_newline.txt', 'w') as f:
+        with redirect_stdout(f):
+            print(res)
+    return res
+
+"""
+This function generates random text using letters and digits from the ascii libraries and adds only whitespaces at random intervals
+and exports the print output as a .txt file
+"""
+def generate_text_spaces():
+    numChars = int(input("Input desired length of string: " ))
+    ref = string.ascii_letters + string.digits + ' '
+    newstr = random.choices(ref, k = numChars)
+    res = ''.join(newstr)
+    with open('out_spaces.txt', 'w') as f:
+        with redirect_stdout(f):
+            print(res)
+    return res

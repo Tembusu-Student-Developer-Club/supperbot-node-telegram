4716e7f8951153e9b89a4a98889f8ad1da671817
Vigneshwar Hariharan <88077703+vigonometry@users.noreply.github.com>
2021-12-09 18:48:02 +0800

    Update chunkify.js

--- helper/chunkify.js
+++ helper/chunkify.js
@@ -1,26 +1,29 @@
 function chunkify(message, maxChars){
-    let lines = message.split(/\n/);
+    const lines = message.split(/\n/);
     let chunks = [];
     let numChars = 0;
     for (let i = 0; i < lines.length; i++){
         numChars = lines[i].length;
-        if (numChars < maxChars){
-            chunks.push(lines[i]);
+        if (line.length + numChars < maxChars){
+            line += lines[i];
+            chunks.push(line);
         } else {
-            let loc = lines[i].lastIndexOf(" ");
-            let subchunk = "";
-            let subsubchunks = [];
-            if (loc >= 0 && loc < maxChars){
-                chunks.push(lines[i].slice(0, loc));
-                subchunk = lines[i].slice(loc);
+            const space_idx = lines[i].lastIndexOf(" ");
+            let subChunk = "";
+            let subSubChunks = [];
+            if (space_idx >= 0 && space_idx < maxChars - line.length){
+                line += lines[i].slice(0, space_idx);
+                chunks.push(line);
+                subChunk = lines[i].slice(space_idx + 1);
             } else {
-                chunks.push(lines[i].slice(0, maxChars - 1));
-                subchunk = lines[i].slice(maxChars - 1);
+                chunks.push(line);
+                chunks.push(lines[i].slice(0, maxChars - 1))
+                subChunk = lines[i].slice(maxChars);
 
             }
-            subsubchunks = chunkify(subchunk, maxChars);
-            for (let j = 0; j < subsubchunks.length; j++) {
-                chunks.push(subsubchunks[j]);
+            subSubChunks = chunkify(subChunk, maxChars);
+            for (let j = 0; j < subSubChunks.length; j++) {
+                chunks.push(subSubChunks[j]);
             }
         }
     }

16e2de5d1c8025626651889aa9e83dec19c62aa7
rushilramesh <rushilramesh@gmail.com>
2021-12-14 16:04:31 +0800

    Implement menu reader

---
+++ menu/reader.js
@@ -0,0 +1,34 @@
+// Import Dependencies
+const fs = require("fs");
+const YAML = require("js-yaml");
+
+class Menu {
+    constructor(text) {
+        this.delivery = text.Delivery;
+        this.instructions = text?.Instructions; // If instructions provided
+        this.dishes = text.Menu;
+        this.modifiers = text.Modifiers
+    }
+
+}
+
+// Convert .yml file to object
+const ymlFileToText = (filename) => {
+    try {
+        const raw = fs.readFileSync(filename);
+        const data = YAML.load(raw)
+        return data
+    } catch (e) {
+        console.log(e)
+    }
+}
+const getMenu = (data) => {
+    const menu = new Menu(data)
+    return menu
+}
+
+
+
+
+
+

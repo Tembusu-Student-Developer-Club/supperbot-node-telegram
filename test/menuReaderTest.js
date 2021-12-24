9ea73540dffaeb1f39ca2ec7edbabadfc8462b04
rushilramesh <rushilramesh@gmail.com>
2021-12-24 13:23:22 +0800

    Add comments to tests

--- test/menuReaderTest.js
+++ test/menuReaderTest.js
@@ -19,7 +19,15 @@ const SAMPLE_MENU = new Menu({
     }
 })
 
+/**
+ * Testing suite for menuReader.js
+ * Method signature for getMenuFromFile is (filename)
+ */
+
 describe('GetMenuFromFile', function() {
+    /**
+     * Tests whether valid yml file is parsed correctly.
+     */
     it('should output valid menu', function(done) {
         const actualMenu = getMenuFromFile('test/testUtil/sampleMenu.yml')
         try {
@@ -31,7 +39,9 @@ describe('GetMenuFromFile', function() {
         }
     })
 
-
+    /**
+     * Tests whether error is thrown for invalid file name. Error is printed to console.
+     */
     it('should throw error with invalid file name', function(done) {
         assert.throws(function () {
             getMenuFromFile('test/testUtil/sampleMen.yml')
@@ -39,6 +49,9 @@ describe('GetMenuFromFile', function() {
         done()
     })
 
+    /**
+     * Tests whether getMenuFromFIle throws an error invalid YML. Error is printed to console.
+     */
     it('should throw yml parsing error', function(done) {
         assert.throws(function () {
             getMenuFromFile('test/testUtil/invalidMenu.yml')
